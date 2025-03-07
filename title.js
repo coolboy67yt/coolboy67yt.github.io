01846ba1de3c2d92278829d3bc497f821798756e
turtledevv <62922384+coolboy67yt@users.noreply.github.com>
2025-03-07 11:48:24 -0600

    aaa a a

--- title.js
+++ title.js
@@ -1,33 +1,19 @@
-let title = "turtldev.online";
+let originalTitle = document.title;  // Grabbing the original title
 let i = 0;
 let isCursor = false;
-let isErasing = false;
 
 function updateTitle() {
-  if (i === title.length && !isErasing) {
-    setTimeout(() => {
-      isErasing = true;
-    }, 5000); // Wait 5 seconds after finishing typing
+  if (i === originalTitle.length) {
+    i = 0; // Reset i to 0 when finished typing
   }
 
-  if (isErasing) {
-    if (i > 0) {
-      i--;
-    } else {
-      isErasing = false;
-    }
-  } else {
-    if (i < title.length) {
-      i++;
-    }
-  }
-
-  let currentText = title.substring(0, i);
-  if (isCursor && i % 2 === 0 && i !== title.length) {
+  let currentText = originalTitle.substring(0, i);
+  if (isCursor && i % 2 === 0) {
     currentText += "_";
   }
 
   document.title = currentText;
+  i++;
 }
 
 setInterval(updateTitle, 200); // Adjust the interval speed (200ms here)
